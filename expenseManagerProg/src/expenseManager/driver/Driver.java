3b81bbb858a3bc883f53e6524461fa3b62cf58d2
kenneth-fernandes <kferna11@binghamton.edu>
2020-04-14 02:03:28 -0700

    Added the basic java application files with README.md

---
+++ expenseManagerProg/src/expenseManager/driver/Driver.java
@@ -0,0 +1,10 @@
+package expenseManager.driver;
+
+/**
+ * @author Kenneth Peter Faernandes
+ */
+public class Driver {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Expense manager program");
+	}
+}
